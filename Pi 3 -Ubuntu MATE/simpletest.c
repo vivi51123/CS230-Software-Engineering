4f6726111c6c0d6dd973797329e205316131e7f6
kunalpatel1793 <kunalpatel1793@gmail.com>
2017-05-22 23:04:55 -0700

    Added Print Statement to test if valgrind shows output

--- Pi 3 -Ubuntu MATE/simpletest.c
+++ Pi 3 -Ubuntu MATE/simpletest.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 void f(void)
 {
     int* x = malloc(10 * sizeof(int));
-    x[10] = 0; // problem 1: heap block overrun
-} // problem 2: memory leak -- x not freed
+    x[10] = 0;        // problem 1: heap block overrun
+}                    // problem 2: memory leak -- x not freed
 
 int main(void)
 {
     f();
+    printf("DONE\n");
     return 0;
 }
